60836b7f4b66d16e7be1d42be6995dc1d26eb9f5
Sohum Gupta <sohumgupta99@gmail.com>
2020-07-08 17:39:23 -0500

    changed font, restarting projects

--- marquee-theme/src/Sections/ProjectsSection/ProjectsSection.js
+++ marquee-theme/src/Sections/ProjectsSection/ProjectsSection.js
@@ -5,21 +5,8 @@ import Project from '../Project/Project';
 
 class ProjectsSection extends React.Component {
 	render() {
-		const renderedProjects = this.props.projects.map((p, i) => 
-			<Project 
-				info={p} 
-				index={i}
-				resize={this.props.resize}/>
-		);
-		const heightStyle = {
-			height: 'calc(' + Math.ceil(this.props.projects.length / 2) + ' * (35vh + 3vw) + 5vh)'
-		}
-
 		return (
-			<div 
-				className={"projects-wrapper" + (this.props.resize ? " resize" : "") } 
-				style={heightStyle}>
-				{renderedProjects}
+			<div>
 			</div>
 		)
 	}
